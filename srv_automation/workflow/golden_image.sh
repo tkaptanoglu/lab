2c0cb8f14178b104dcb1096aba05abe35e355419
Tigin Kaptanoglu <tiginkaptanoglu@gmail.com>
2026-01-17 16:07:00 +0100

    Ensuring log format consistency in the Golden Image workflow

--- srv_automation/workflow/golden_image.sh
+++ srv_automation/workflow/golden_image.sh
@@ -4,20 +4,22 @@
 
 set -euo pipefail
 
+echo "=== Golden Image creation started ==="
+
 # NOTE: If you wish to redefine what 'necessary package' means, please modify the relevant atomics.
-echo "=== Step 1/4: Install necessary packages"
+echo "--- Step 1/4: Install necessary packages ---"
 ./srv_automation/composite/install_necessary_tools.sh
 
 # Security baselines: Automated patching, VPN, firewall, SSH hardening
-echo "=== Step 2/4: Apply security baselines"
+echo "--- Step 2/4: Apply security baselines ---"
 ./srv_automation/composite/apply_security_baselines.sh
 
 # Generic network config: Hostname, DNS, generic sysctl tuning
-echo "=== Step 3/4: Configure generic network"
+echo "--- Step 3/4: Configure generic network ---"
 ./srv_automation/composite/generic_network_config.sh
 
 # System configuration tweaks
-echo "=== Step 4/4: System configuration tweaks"
+echo "--- Step 4/4: System configuration tweaks ---"
 ./srv_automation/composite/system_config_tweaks.sh
 
 echo "=== Golden Image creation complete. ==="
