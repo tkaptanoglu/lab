c2a0ae101d26cde533ea4071c41313239671be80
Tigin Kaptanoglu <tiginkaptanoglu@gmail.com>
2026-01-17 15:35:08 +0100

    Adding Vault installation to the preinit workflow

--- srv_automation/workflow/preinit.sh
+++ srv_automation/workflow/preinit.sh
@@ -6,22 +6,26 @@ set -euo pipefail
 
 echo "=== Preinit workflow started ==="
 
-echo "--- Step 1/5: Wait for cloud-init to finish ---"
+echo "--- Step 1/6: Wait for cloud-init to finish ---"
 ./srv_automation/atomic/wait_for_cloud_init.sh
 
-echo "--- Step 2/5: Update and upgrade system packages ---"
+echo "--- Step 2/6: Update and upgrade system packages ---"
 ./srv_automation/atomic/update_and_upgrade.sh
 
-echo "--- Step 3/5: Ensure scripts have executable permissions ---"
+echo "--- Step 3/6: Ensure scripts have executable permissions ---"
 ./srv_automation/composite/set_script_runnable_permissions.sh
 
-echo "--- Step 4/5: Enable cgroups (requires reboot) ---"
+# Vault installation is a part of preinit because other scripts will need secrets.
+echo "--- Step 4/6: Install Vault ---"
+./srv_automation/atomic/install_vault.sh
+
+echo "--- Step 5/6: Enable cgroups (requires reboot) ---"
 ./srv_automation/atomic/enable_cgroups.sh
 
 # NOTE:
 # enable_cgroups.sh may already reboot the system.
 # If it does NOT reboot, this ensures a reboot happens.
 
-echo "--- Step 5/5: Reboot the system ---"
+echo "--- Step 5/6: Reboot the system ---"
 ./srv_automation/atomic/reboot.sh
 
